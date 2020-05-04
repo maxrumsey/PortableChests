97fcbc52b7f72bdb4f3fdb96c97c785baafb3bb9
Max Rumsey <22051776+maxrumsey@users.noreply.github.com>
2020-05-04 21:01:01 +0800

    Adding brigadier support

--- src/main/java/xyz/maxrumsey/portablechests/BrigadierSetup.java
+++ src/main/java/xyz/maxrumsey/portablechests/BrigadierSetup.java
@@ -6,6 +6,7 @@ import me.lucko.commodore.file.CommodoreFileFormat;
 import org.bukkit.command.PluginCommand;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class BrigadierSetup {
     static public void setup(Commodore commodore, PortableChests plugin) {
@@ -16,8 +17,8 @@ public class BrigadierSetup {
     }
     private static void command(PortableChests plugin, String name, Commodore commodore) {
         try {
-            PluginCommand bukkitCommand = plugin.getCommand(name);
-            LiteralCommandNode<?> command = CommodoreFileFormat.parse(plugin.getResource("commodore/" + name + ".commodore"));
+            PluginCommand bukkitCommand = Objects.requireNonNull(plugin.getCommand(name));
+            LiteralCommandNode<?> command = CommodoreFileFormat.parse(Objects.requireNonNull(plugin.getResource(name + ".commodore")));
             commodore.register(bukkitCommand, command);
         } catch (IOException e) {
             System.out.println(e.getMessage());
